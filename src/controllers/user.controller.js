915703454ab5166db7160af01272faa0464000cc
Sanjyoshree <jyotikasinha21@gmail.com>
2025-03-11 12:31:00 +0530

    add user account management endpoints

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -232,11 +232,48 @@ const changeCurrentPassword = asyncHandler(async(req,res) => {
       if(!isPasswordCorrect) {
             throw new ApiError(400, "Invalid old password")
       }
+
+      user.password = newPassword
+      await user.save ({validateBeforeSave: false})
+
+      return res
+      .status(200)
+      .json(new ApiResponse(200 , {} , "Password changed successfully"))
+
+})
+
+const getCurrentUser = asyncHandler(async(req , res)=>{
+      return res
+      .status(200)
+      .json(200, req.user , "current user fetched successfully")
+})
+
+const updateAccountDetails = asyncHandler(async(req , res) => {
+      const {fullName, email} = req.body 
+      //if you're uploading or taking files then keep a seperate controller for that
+
+      if(!fullName || !email) {
+            throw new ApiError(400, "All feilds are required")
+      }
+
+      const user = User.findByIdAndUpdate(
+            req.user?._id,
+            {
+                  $set: {
+                        fullName,
+                        email: email
+                  }
+
+            },
+            {new: true}
+      ).select("-password")
+
 })
 
 export {
       registerUser,
       loginUser,
       logoutUser,
-      refreshAccessToken
+      refreshAccessToken,
+      getCurrentUser
 }
